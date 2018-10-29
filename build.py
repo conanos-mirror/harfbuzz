75044acbea9ff00e2ccbb2d78a9760a32e7db900
Mingyi Zhang <zhangmingyi@kedacom.com>
2018-10-29 14:15:31 +0800

    set defualt channel to conanos

--- build.py
+++ build.py
@@ -3,9 +3,11 @@
 
 
 from bincrafters import build_template_default
+import os
+
+os.environ['CONAN_USERNAME'] = os.environ.get('CONAN_USERNAME','conanos')
 
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder()
-
-builder.run()
+    builder.run()
