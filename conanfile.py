0bfc6326a25be5248893f24dbe606e3c20eb6905
cppbitman <chinadaihongjun@gmail.com>
2018-11-01 05:11:29 -0700

    add dependency pixman libpng for hb-ft.h

--- conanfile.py
+++ conanfile.py
@@ -17,9 +17,13 @@ class HarfbuzzConan(ConanFile):
         'fPIC': [True, False],
         'with_freetype': [True, False]
     }
-    default_options = ("shared=True", "fPIC=True", "with_freetype=False")
+    default_options = ("shared=True", "fPIC=True", "with_freetype=True")
     generators = "cmake"
-    requires = ("fontconfig/2.12.6@conanos/dev","cairo/1.14.12@conanos/dev","glib/2.58.0@conanos/dev",)
+    requires = ("fontconfig/2.12.6@conanos/dev","cairo/1.14.12@conanos/dev","glib/2.58.0@conanos/dev",
+    "freetype/2.9.0@conanos/dev",
+    "pixman/0.34.0@conanos/dev",#required by 'cairo' 
+    "libpng/1.6.34@conanos/dev"#required by 'freetype2'
+    )
 
     source_subfolder = "source_subfolder"
 
@@ -32,17 +36,25 @@ class HarfbuzzConan(ConanFile):
     def build(self):
         with tools.chdir(self.source_subfolder):
             with tools.environment_append({
-                'PKG_CONFIG_PATH': ':%s/lib/pkgconfig:%s/lib/pkgconfig:%s/lib/pkgconfig'
+                'PKG_CONFIG_PATH': ':%s/lib/pkgconfig:%s/lib/pkgconfig:%s/lib/pkgconfig:%s/lib/pkgconfig:%s/lib/pkgconfig:%s/lib/pkgconfig'
                 %(self.deps_cpp_info['fontconfig'].rootpath,
                 self.deps_cpp_info['cairo'].rootpath,
-                self.deps_cpp_info['glib'].rootpath,)}):
+                self.deps_cpp_info['glib'].rootpath,
+                self.deps_cpp_info['freetype'].rootpath,
+                self.deps_cpp_info['pixman'].rootpath,
+                self.deps_cpp_info['libpng'].rootpath,
+                )}):
                 
                 _args = ['--prefix=%s/builddir'%(os.getcwd()), '--libdir=%s/builddir/lib'%(os.getcwd()),
-                    '--disable-silent-rules', '--enable-introspection', '--with-icu=no']
+                    '--disable-silent-rules', '--enable-introspection', '--with-icu=no',]
                 if self.options.shared:
                     _args.extend(['--enable-shared=yes','--enable-static=no'])
                 else:
                     _args.extend(['--enable-shared=no','--enable-static=yes'])
+                if self.options.with_freetype:
+                    _args.extend(['--with-freetype=yes'])
+                else:
+                    _args.extend(['--with-freetype=no'])
 
                 self.run('./configure %s'%(' '.join(_args)))#space
                 self.run('make -j2')
